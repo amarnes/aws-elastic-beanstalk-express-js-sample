35b3a64c8835826e2ba9694d10bc0ee278aebb4c
amarnes <Alejandro.Arnes@and.digital>
2021-03-14 11:24:27 +0000

    Again and again...

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Saluto Mondo!'));
+app.get('/', (req, res) => res.send('Saluto Mondo!...again and again and again and again...'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
